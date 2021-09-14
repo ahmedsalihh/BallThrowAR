618d80f1e8c68eabd9fb2d747cd8a5ab89d970f3
ahmedsalihh <ahmedsalihh@gmail.com>
2021-09-15 00:44:14 +0300

    Add spawn points and balloon prefabs && Add spawn script and spawn balloons

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject[] balloons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawn());
+    }
+
+    IEnumerator StartSpawn() {
+        yield return new WaitForSeconds(4);
+
+        for (int i = 0; i < 3; i++) {
+            Instantiate(balloons[i], spawnPoints[i].position, Quaternion.identity);
+        }
+
+        StartCoroutine(StartSpawn());
+    }
+}
